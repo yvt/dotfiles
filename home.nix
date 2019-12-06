4d013befcaa002f5510b8810a9f33f746b30be2b
yvt <i@yvt.jp>
2019-12-06 16:35:14 +0900

    Add fish abbreviations

--- home.nix
+++ home.nix
@@ -4,6 +4,10 @@ with builtins;
 with lib;
 with import <home-manager/modules/lib/dag.nix> { inherit lib; };
 
+let
+  isDarwin = hasSuffix "-darwin" currentSystem;
+in
+
 {
   imports = filter (e: e != null) [
     (if pathExists ./private then ./private/home.nix else null)
@@ -11,4 +15,34 @@ with import <home-manager/modules/lib/dag.nix> { inherit lib; };
   ];
 
   programs.home-manager.enable = true;
+
+  programs.fish.enable = true;
+  programs.fish.shellAbbrs = {
+    "e" = "edit";
+    "g-s" = "git status";
+    "g-c" = "git commit";
+    "gi" = "git issue";
+    "gil" = "git issue list -l short";
+  }
+  // optionalAttrs (!isDarwin) {
+    "-jc" = "journalctl";
+    "jc-x" = "journalctl -xe";
+    "-sc" = "systemctl";
+    "sc-t" = "systemctl start";
+    "sc-p" = "systemctl stop";
+    "sc-r" = "systemctl restart";
+    "sc-s" = "systemctl status";
+  };
+
+  # TODO: replace `TERM=screen` with `TERM=screen-256color`?
+  # TODO: check `EDITOR` variable
+  # TODO: add PATHs
+  # TODO: add LSD
+  # TODO: add powerline-rs
+
+  # configure PATH and other variables to use Nix
+  programs.fish.loginShellInit =
+    ''
+      source ${./fish/nix.fish}
+    '';
 }
