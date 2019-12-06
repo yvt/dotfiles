8c765731468ca5273b95c95a311ce54cdb558928
yvt <i@yvt.jp>
2019-12-06 18:41:16 +0900

    Improve shell boot time by caching pipenv's autocompletion setup

--- modules/programs/pipenv.nix
+++ modules/programs/pipenv.nix
@@ -1,9 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+
+with lib;
+
+let
+  stdenv = (import <nixpkgs> {}).stdenv;
+
+  pipenvFishAutocompletion = stdenv.mkDerivation {
+    name = "pipenv-fish-completion";
+    dontUnpack = true;
+    buildInputs = [ pkgs.pipenv ];
+    buildPhase = ''
+      _PIPENV_COMPLETE=source-fish pipenv > setenv.fish
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp setenv.fish $out/bin
+    '';
+  };
+in
 {
   home.packages = [ pkgs.pipenv ];
 
   programs.fish.interactiveShellInit =
-    ''
-    eval (env _PIPENV_COMPLETE=source-fish ${pkgs.pipenv}/bin/pipenv)
-    '';
+    replaceStrings ["pipenv)"] ["${pkgs.pipenv}/bin/pipenv)"]
+      (readFile "${pipenvFishAutocompletion}/bin/setenv.fish");
 }
