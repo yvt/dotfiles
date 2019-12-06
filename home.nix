1b5d8836823bfd43db0b06c2f608c88819061b9c
yvt <i@yvt.jp>
2019-12-06 16:17:56 +0900

    Import private & local config

--- home.nix
+++ home.nix
@@ -5,5 +5,10 @@ with lib;
 with import <home-manager/modules/lib/dag.nix> { inherit lib; };
 
 {
+  imports = filter (e: e != null) [
+    (if pathExists ./private then ./private/home.nix else null)
+    (if pathExists ./local then ./local/home.nix else null)
+  ];
+
   programs.home-manager.enable = true;
 }
