3f6d301bcacd63d566464f1bd04720024eaea2fa
yvt <i@yvt.jp>
2020-01-08 13:13:17 +0900

    lsd: Remove shell aliases for fish now that they are merged to upstream
    <https://github.com/rycee/home-manager/pull/974>

--- modules/programs/lsd.nix
+++ modules/programs/lsd.nix
@@ -5,13 +5,4 @@
     enable = true;
     enableAliases = true;
   };
-
-  # <home-manager/modules/programs/lsd.nix> does not come with fish aliases
-  programs.fish.shellAliases = {
-    ls = "${pkgs.lsd}/bin/lsd";
-    ll = "ls -l";
-    la = "ls -a";
-    lt = "ls --tree";
-    lla ="ls -la";
-  };
 }
