01c23ea4344ac8f2ae2079a88d89874745063185
yvt <i@yvt.jp>
2021-11-27 17:05:15 +0900

    `programs.fish.promptInit` was superseded by `programs.fish.interactiveShellInit`
    This change was necessitated by the following PR:
    <https://github.com/nix-community/home-manager/pull/2231>

--- modules/programs/powerline-rs.nix
+++ modules/programs/powerline-rs.nix
@@ -17,7 +17,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.fish.promptInit =
+    programs.fish.interactiveShellInit =
       let
         modules = concatStringsSep "," cfg.modules;
       in
